8e787dd463c4c76a6b36bdefd9269f686afcb883
Peng Jie <bivinity.pengzjie@gmail.com>
2018-02-18 12:41:58 +0800

    Update tests

--- src/components/__tests__/VueSignatruePad.spec.js
+++ src/components/__tests__/VueSignatruePad.spec.js
@@ -1,11 +1,58 @@
-import { mount } from '@vue/test-utils';
+import { shallow } from '@vue/test-utils';
 import VueSignaturePad from '../VueSignaturePad';
-
-const Wrapper = mount(VueSignaturePad);
-const vm = Wrapper.vm;
+import { signatureMockData, mockEncodeDataURL } from './mock';
 
 describe('VueSignaturePad Component', () => {
-  it('should be pass', () => {
-    expect(true).toBe(true);
+  it('should be receive default props', () => {
+    const wrapper = shallow(VueSignaturePad);
+
+    const expectedWidth = '100%';
+    const expectedHeight = '100%';
+    const expectedSaveType = 'image/png';
+    const expectedOptions = {};
+    const expectedImages = [];
+
+    expect(wrapper.props().width).toBe(expectedWidth);
+    expect(wrapper.props().height).toBe(expectedHeight);
+    expect(wrapper.props().options).toEqual(expectedOptions);
+    expect(wrapper.props().images).toEqual(expectedImages);
+  });
+
+  it('should be throw incorrect image error message', () => {
+    const addOptionsWrapper = shallow(VueSignaturePad, {
+      propsData: {
+        saveType: 'text/html'
+      }
+    });
+
+    expect(() => addOptionsWrapper.vm.saveSignature()).toThrow();
+  });
+
+  it('should be return save signaturePad status', () => {
+    const wrapper = shallow(VueSignaturePad);
+
+    wrapper.vm.saveSignature();
+    expect(wrapper.vm.saveSignature()).toEqual({
+      isEmpty: true,
+      data: undefined
+    });
+    wrapper.vm.signaturePad._data = signatureMockData;
+
+    wrapper.setData({
+      signaturePad: {
+        ...wrapper.vm.signaturePad,
+        _data: signatureMockData,
+        isEmpty() {
+          return false;
+        },
+        toDataURL(type, ...options) {
+          return mockEncodeDataURL;
+        }
+      }
+    });
+    expect(wrapper.vm.saveSignature()).toEqual({
+      isEmpty: false,
+      data: mockEncodeDataURL
+    });
   });
 });
