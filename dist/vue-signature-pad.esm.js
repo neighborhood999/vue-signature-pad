6b086df4c3574135debfeb530f9005dbc16dd76b
Peng Jie <dean.leehom@gmail.com>
2018-03-13 10:09:43 +0800

    Update dependencies (#26)

--- dist/vue-signature-pad.esm.js
+++ dist/vue-signature-pad.esm.js
@@ -1,1 +1,182 @@
-import SignaturePad from"signature_pad";import mergeImages from"merge-images";var SAVE_TYPE=["image/png","image/jpeg","image/svg+xml"],checkSaveType=function(e){return SAVE_TYPE.includes(e)},DEFAULT_OPTIONS={minWidth:.5,maxWidth:2.5,throttle:16,minDistance:5,backgroundColor:"rgba(0,0,0,0)",penColor:"black",velocityFilterWeight:.7},convert2NonReactive=function(e){return JSON.parse(JSON.stringify(e))},TRANSPARENT_PNG={src:"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=",x:0,y:0},VueSignaturePad={name:"VueSignaturePad",props:{width:{type:String,default:"100%"},height:{type:String,default:"100%"},customStyle:{type:Object},saveType:{type:String,default:"image/png"},options:{type:Object,default:function(){return{}}},images:{type:Array,default:function(){return[]}}},data:function(){return{signaturePad:{},cacheImages:[],signatureData:TRANSPARENT_PNG}},mounted:function(){var e=this.options,t=this.$refs.signaturePadCanvas,a=new SignaturePad(t,Object.assign({},DEFAULT_OPTIONS,e));this.signaturePad=a,window.addEventListener("resize",this.resizeCanvas.bind(this,t),!1),this.resizeCanvas(t)},methods:{resizeCanvas:function(e){var t=Math.max(window.devicePixelRatio||1,1);e.width=e.offsetWidth*t,e.height=e.offsetHeight*t,e.getContext("2d").scale(t,t),this.signaturePad.clear(),this.signatureData=TRANSPARENT_PNG},saveSignature:function(){var e=this.signaturePad,t=this.saveType,a={isEmpty:!1,data:void 0};if(!checkSaveType(t))throw new Error("Image type is incorrect!");return e.isEmpty()?Object.assign({},a,{isEmpty:!0}):(this.signatureData=e.toDataURL(t),Object.assign({},a,{data:this.signatureData}))},undoSignature:function(){var e=this.signaturePad,t=e.toData();if(t)return e.fromData(t.slice(0,-1))},mergeImageAndSignature:function(e){return this.signatureData=e,mergeImages(this.images.concat(this.cacheImages,[this.signatureData]))},addImages:function(e){return void 0===e&&(e=[]),this.cacheImages=this.cacheImages.concat(e),mergeImages(this.images.concat(this.cacheImages,[this.signatureData]))},lockSignaturePad:function(){return this.signaturePad.off()},openSignaturePad:function(){return this.signaturePad.on()},getPropImagesAndCacheImages:function(){return this.propsImagesAndCustomImages},clearCacheImages:function(){this.cacheImages=[]}},computed:{propsImagesAndCustomImages:function(){var e=convert2NonReactive(this.images),t=convert2NonReactive(this.cacheImages);return e.concat(t)}},render:function(e){var t=this.width,a=this.height,i=this.customStyle;return e("div",{style:Object.assign({},{width:t,height:a},i)},[e("canvas",{style:{width:"100%",height:"100%"},ref:"signaturePadCanvas"})])},install:function(e){return e.component(VueSignaturePad.name,VueSignaturePad)}};"undefined"!=typeof window&&window.Vue&&window.Vue.use(VueSignaturePad);export default VueSignaturePad;
+import SignaturePad from 'signature_pad';
+import mergeImages from 'merge-images';
+
+var SAVE_TYPE = ['image/png', 'image/jpeg', 'image/svg+xml'];
+
+var checkSaveType = function (type) { return SAVE_TYPE.includes(type); };
+
+var DEFAULT_OPTIONS = {
+  minWidth: 0.5,
+  maxWidth: 2.5,
+  throttle: 16,
+  minDistance: 5,
+  backgroundColor: 'rgba(0,0,0,0)',
+  penColor: 'black',
+  velocityFilterWeight: 0.7
+};
+
+var convert2NonReactive = function (observerValue) { return JSON.parse(JSON.stringify(observerValue)); };
+
+var TRANSPARENT_PNG = {
+  src:
+    'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=',
+  x: 0,
+  y: 0
+};
+
+var VueSignaturePad = {
+  name: 'VueSignaturePad',
+  props: {
+    width: {
+      type: String,
+      default: '100%'
+    },
+    height: {
+      type: String,
+      default: '100%'
+    },
+    customStyle: {
+      type: Object
+    },
+    saveType: {
+      type: String,
+      default: 'image/png'
+    },
+    options: {
+      type: Object,
+      default: function () { return ({}); }
+    },
+    images: {
+      type: Array,
+      default: function () { return []; }
+    }
+  },
+  data: function () { return ({
+    signaturePad: {},
+    cacheImages: [],
+    signatureData: TRANSPARENT_PNG
+  }); },
+  mounted: function mounted() {
+    var ref = this;
+    var options = ref.options;
+    var canvas = this.$refs.signaturePadCanvas;
+    var signaturePad = new SignaturePad(canvas, Object.assign({}, DEFAULT_OPTIONS,
+      options));
+    this.signaturePad = signaturePad;
+
+    window.addEventListener(
+      'resize',
+      this.resizeCanvas.bind(this, canvas),
+      false
+    );
+
+    this.resizeCanvas(canvas);
+  },
+  methods: {
+    resizeCanvas: function resizeCanvas(canvas) {
+      var ratio = Math.max(window.devicePixelRatio || 1, 1);
+      canvas.width = canvas.offsetWidth * ratio;
+      canvas.height = canvas.offsetHeight * ratio;
+      canvas.getContext('2d').scale(ratio, ratio);
+      this.signaturePad.clear();
+      this.signatureData = TRANSPARENT_PNG;
+    },
+    saveSignature: function saveSignature() {
+      var ref = this;
+      var signaturePad = ref.signaturePad;
+      var saveType = ref.saveType;
+      var status = { isEmpty: false, data: undefined };
+
+      if (!checkSaveType(saveType)) {
+        throw new Error('Image type is incorrect!');
+      }
+
+      if (signaturePad.isEmpty()) {
+        return Object.assign({}, status,
+          {isEmpty: true});
+      } else {
+        this.signatureData = signaturePad.toDataURL(saveType);
+
+        return Object.assign({}, status,
+          {data: this.signatureData});
+      }
+    },
+    undoSignature: function undoSignature() {
+      var ref = this;
+      var signaturePad = ref.signaturePad;
+      var record = signaturePad.toData();
+
+      if (record) {
+        return signaturePad.fromData(record.slice(0, -1));
+      }
+    },
+    mergeImageAndSignature: function mergeImageAndSignature(customSignature) {
+      this.signatureData = customSignature;
+
+      return mergeImages(this.images.concat( this.cacheImages,
+        [this.signatureData]
+      ));
+    },
+    addImages: function addImages(images) {
+      if ( images === void 0 ) images = [];
+
+      this.cacheImages = this.cacheImages.concat( images);
+
+      return mergeImages(this.images.concat( this.cacheImages,
+        [this.signatureData]
+      ));
+    },
+    lockSignaturePad: function lockSignaturePad() {
+      return this.signaturePad.off();
+    },
+    openSignaturePad: function openSignaturePad() {
+      return this.signaturePad.on();
+    },
+    getPropImagesAndCacheImages: function getPropImagesAndCacheImages() {
+      return this.propsImagesAndCustomImages;
+    },
+    clearCacheImages: function clearCacheImages() {
+      this.cacheImages = [];
+    }
+  },
+  computed: {
+    propsImagesAndCustomImages: function propsImagesAndCustomImages() {
+      var nonReactiveProrpImages = convert2NonReactive(this.images);
+      var nonReactiveCachImages = convert2NonReactive(this.cacheImages);
+
+      return nonReactiveProrpImages.concat( nonReactiveCachImages);
+    }
+  },
+  render: function render(createElement) {
+    var ref = this;
+    var width = ref.width;
+    var height = ref.height;
+    var customStyle = ref.customStyle;
+
+    return createElement(
+      'div',
+      {
+        style: Object.assign({}, {width: width,
+          height: height},
+          customStyle)
+      },
+      [
+        createElement('canvas', {
+          style: {
+            width: '100%',
+            height: '100%'
+          },
+          ref: 'signaturePadCanvas'
+        })
+      ]
+    );
+  }
+};
+
+VueSignaturePad.install = function (Vue) { return Vue.component(VueSignaturePad.name, VueSignaturePad); };
+
+if (typeof window !== 'undefined' && window.Vue) {
+  window.Vue.use(VueSignaturePad);
+}
+
+export default VueSignaturePad;
