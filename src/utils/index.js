d987179af6fa3c53a4d2ccce9446d2ad9ffba44c
Peng Jie <bivinity.pengzjie@gmail.com>
2018-09-20 14:45:01 +0800

    enhance: update options

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,6 +3,7 @@ const SAVE_TYPE = ['image/png', 'image/jpeg', 'image/svg+xml'];
 export const checkSaveType = type => SAVE_TYPE.includes(type);
 
 export const DEFAULT_OPTIONS = {
+  dotSize: (0.5 + 2.5) / 2,
   minWidth: 0.5,
   maxWidth: 2.5,
   throttle: 16,
