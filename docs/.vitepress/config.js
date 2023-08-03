6ad06eeaee0611d0eec0640061d02b41084eec6a
pratikpatel <pratikpatelpp802@gmail.com>
2023-08-03 16:11:49 +0530

    Document added

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,51 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Vue Signature Pad",
+  description: "Vue Signature Pad Component",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/vue3-example' },
+      { text: 'Support', link: '/team' }
+    ],
+
+    search: {
+        provider: 'local'
+    },
+
+    footer: {
+        copyright: 'CC-BY / MIT License | Copyright © 2023 Peng Jie'
+    },
+    sidebar: [
+
+      {
+        text: 'Introduction',
+        items: [
+          { text: 'Getting Started', link: '/getting-started' }
+        ]
+      },
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Vue 3 Example', link: '/vue3-example' },
+          { text: 'Vue 3 Single Page Component', link: '/vue3-single-page-component-example' },
+          { text: 'Vue 2 Example', link: '/vue2-example' },
+          { text: 'Call Back Example', link: '/callback-example' }
+        ]
+      },
+      {
+          text: 'Support',
+          items: [
+              {text: 'Teams', link: '/team'},
+          ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/neighborhood999/vue-signature-pad' }
+    ]
+  }
+})
---
+++ docs/index.md
@@ -0,0 +1,27 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "Vue Signature Pad"
+  text: "Vue Signature <br/>Pad Component"
+  actions:
+    - theme: brand
+      text: Getting Started
+      link: /getting-started
+    - theme: alt
+      text: Examples
+      link: /vue3-example
+
+features:
+  - title: Vue Signature Pad Component
+    details: Easy-to-use for Vue 2/3.
+    icon: '🚀'
+  - title: Export as Image
+    details: Save signature data effortlessly.
+    icon: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M5 21q-.825 0-1.413-.588T3 19V5q0-.825.588-1.413T5 3h14q.825 0 1.413.588T21 5v14q0 .825-.588 1.413T19 21H5Zm1-4h12l-3.75-5l-3 4L9 13l-3 4Z"/></svg>'
+  - title: Merge Image And Signature
+    details: Simplifying image merging with signatures.  
+    icon: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M22 16V4a2 2 0 0 0-2-2H8a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2m-11-4l2.03 2.71L16 11l4 5H8M2 6v14a2 2 0 0 0 2 2h14v-2H4V6"/></svg>'
+---
+
